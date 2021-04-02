44cc7314a4ff539bfad20f54c093e15b5d32d9ce
Jamie <jamie-antoine.ponchon@epitech.eu>
2021-04-02 10:52:05 +0200

    correcting random spawns for food

--- games/nibler/nibler.cpp
+++ games/nibler/nibler.cpp
@@ -62,7 +62,7 @@ int nibler::mooveSnake(IGraphicLib *GraphicLib)
         int nx = snake[snake.size() - 1]->x;
         int ny = snake[snake.size() - 1]->y;
         delete (pfood);
-        pfood = new player(rand() % 30 + 10, rand() % 30 + 10, "F");
+        pfood = new player(rand() % 23 + 8, rand() % 23 + 8, "F");
         if (dir == 1)
             snake.push_back(new player(nx, ny + 1, "X"));
         if (dir == 2)
